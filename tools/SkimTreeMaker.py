e67ed2665d37587583951e3a31b2adffe67132bd
Sam Bein <sbein@users.noreply.github.com>
2018-10-28 02:20:21 +0100

    Update SkimTreeMaker.py
    updated skim code to accommodate full BDT

--- tools/SkimTreeMaker.py
+++ tools/SkimTreeMaker.py
@@ -8,17 +8,16 @@ from ROOT import *
 from utils import *
 import os, sys
 from glob import glob
-#csv_b = 0.8484
-csv_b = 0.8838
+csv_b = 0.8484
 
-isDasAndSignal = True
+isDasAndSignal = False
 #cross sections can be looked up on the SUSY xsec working group page:
 #https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections#Cross_sections_for_various_S_AN2
 if isDasAndSignal: xsecInPb = 0.00276133
 
 #smdir = '/nfs/dust/cms/user/beinsam/CommonNtuples/MC_SM/'
 #smdir = '/pnfs/desy.de/cms/tier2/store/user/sbein/CommonNtuples/'
-
+print 'isDasAndSignal?', isDasAndSignal
 ##########################################################
 # files specified with optional wildcards @ command line #
 ##########################################################
@@ -41,26 +40,39 @@ histoStyler(hHt,kBlack)
 import numpy as np
 var_Met = np.zeros(1,dtype=float)
 var_Mht = np.zeros(1,dtype=float)
-var_Mt2 = np.zeros(1,dtype=float)
 var_Ht = np.zeros(1,dtype=float)
 var_MinDeltaPhiMhtJets = np.zeros(1,dtype=float)
 var_NJets = np.zeros(1,dtype=int)
 var_BTags = np.zeros(1,dtype=int)
 var_NLeptons = np.zeros(1,dtype=int)
 var_NPhotons = np.zeros(1,dtype=int)
+var_NTags = np.zeros(1,dtype=int)
 var_NPixelTags = np.zeros(1,dtype=int)
 var_NPixelStripsTags = np.zeros(1,dtype=int)
-var_NTags = np.zeros(1,dtype=int)
-var_NPrompt = np.zeros(1,dtype=int)
 var_DPhiMetSumTags = np.zeros(1,dtype=float)
-var_TrackBdtScore = np.zeros(1,dtype=float)
-var_TrackDeDx = np.zeros(1,dtype=float)
-var_TrackDxy = np.zeros(1,dtype=float)
-var_TrackChisquare = np.zeros(1,dtype=float)
+var_Track1BdtScore = np.zeros(1,dtype=float)
+var_Track1Dedx = np.zeros(1,dtype=float)
+var_Track1Dxy = np.zeros(1,dtype=float)
+var_Track1Chisquare = np.zeros(1,dtype=float)
+var_Track1Pt = np.zeros(1,dtype=float)
+var_Track1Eta = np.zeros(1,dtype=float)
+var_Track1Phi = np.zeros(1,dtype=float)
+var_Track2Phi = np.zeros(1,dtype=float)
+var_Track2BdtScore = np.zeros(1,dtype=float)
+var_Track1Dedx = np.zeros(1,dtype=float)
+var_Track2Dedx = np.zeros(1,dtype=float)
+var_Track1MassFromDedx = np.zeros(1,dtype=float)
+var_Track2MassFromDedx = np.zeros(1,dtype=float)
+var_Track2Dxy = np.zeros(1,dtype=float)
+var_Track2Chisquare = np.zeros(1,dtype=float)
+var_Track2Pt = np.zeros(1,dtype=float)
+var_Track2Eta = np.zeros(1,dtype=float)
+var_Track1IsPixelStrips = np.zeros(1,dtype=int)
+var_Track2IsPixelStrips = np.zeros(1,dtype=int)
+var_Track1IsGenMatched = np.zeros(1,dtype=int)
+var_Track2IsGenMatched = np.zeros(1,dtype=int)
+
 var_SumTagPtOverMht = np.zeros(1,dtype=float)
-var_TrackPt = np.zeros(1,dtype=float)
-var_TrackP = np.zeros(1,dtype=float)
-var_TrackEta = np.zeros(1,dtype=float)
 var_CrossSection = np.zeros(1,dtype=float)
 if isDasAndSignal: var_weight = np.zeros(1,dtype=float)
 
@@ -70,25 +82,38 @@ if isDasAndSignal: var_weight = np.zeros(1,dtype=float)
 tEvent = TTree('tEvent','tEvent')
 tEvent.Branch('Met', var_Met,'Met/D')
 tEvent.Branch('Mht', var_Mht,'Mht/D')
-tEvent.Branch('Mt2', var_Mt2,'Mt2/D')
 tEvent.Branch('Ht', var_Ht,'Ht/D')
-tEvent.Branch('MinDeltaPhiMetJets', var_MinDeltaPhiMhtJets,'MinDeltaPhiMetJets/D')
+tEvent.Branch('MinDeltaPhiMhtJets', var_MinDeltaPhiMhtJets,'MinDeltaPhiMhtJets/D')
 tEvent.Branch('NJets', var_NJets,'NJets/I')
 tEvent.Branch('BTags', var_BTags,'BTags/I')
 tEvent.Branch('NLeptons', var_NLeptons,'NLeptons/I')
 tEvent.Branch('NPhotons', var_NPhotons,'NPhotons/I')
+tEvent.Branch('NTags', var_NTags,'NTags/I')
 tEvent.Branch('NPixelTags', var_NPixelTags,'NPixelTags/I')
 tEvent.Branch('NPixelStripsTags', var_NPixelStripsTags,'NPixelStripsTags/I')
-tEvent.Branch('NTags', var_NTags,'NTags/I')
-tEvent.Branch('NPrompt', var_NPrompt,'NPrompt/I')
-tEvent.Branch('TrackPt', var_TrackPt,'TrackPt/D')
-tEvent.Branch('TrackP', var_TrackP,'TrackP/D')
-tEvent.Branch('TrackEta', var_TrackEta,'TrackEta/D')
+tEvent.Branch('Track1Pt', var_Track1Pt,'Track1Pt/D')
+tEvent.Branch('Track2Pt', var_Track2Pt,'Track2Pt/D')
+tEvent.Branch('Track1Eta', var_Track1Eta,'Track1Eta/D')
+tEvent.Branch('Track2Eta', var_Track2Eta,'Track2Eta/D')
+tEvent.Branch('Track1Phi', var_Track1Phi,'Track1Phi/D')
+tEvent.Branch('Track2Phi', var_Track1Phi,'Track2Phi/D')
+tEvent.Branch('Track1BdtScore', var_Track1BdtScore,'Track1BdtScore/D')
+tEvent.Branch('Track2BdtScore', var_Track2BdtScore,'Track2BdtScore/D')
+tEvent.Branch('Track1Chisquare', var_Track1Chisquare,'Track1Chisquare/D')
+tEvent.Branch('Track2Chisquare', var_Track2Chisquare,'Track2Chisquare/D')
+tEvent.Branch('Track1Dxy', var_Track1Dxy,'Track1Dxy/D')
+tEvent.Branch('Track2Dxy', var_Track1Dxy,'Track2Dxy/D')
+tEvent.Branch('Track1Dedx', var_Track1Dedx,'Track1Dedx/D')
+tEvent.Branch('Track2Dedx', var_Track2Dedx,'Track2Dedx/D')
+tEvent.Branch('Track1MassFromDedx', var_Track1MassFromDedx,'Track1MassFromDedx/D')
+tEvent.Branch('Track2MassFromDedx', var_Track2MassFromDedx,'Track2MassFromDedx/D')
+tEvent.Branch('Track1IsPixelStrips', var_Track1IsPixelStrips,'Track1IsPixelStrips/I')
+tEvent.Branch('Track2IsPixelStrips', var_Track2IsPixelStrips,'Track2IsPixelStrips/I')
+tEvent.Branch('Track1IsGenMatched', var_Track1IsGenMatched,'Track1IsGenMatched/I')
+tEvent.Branch('Track2IsGenMatched', var_Track2IsGenMatched,'Track2IsGenMatched/I')
 tEvent.Branch('SumTagPtOverMht', var_SumTagPtOverMht,'SumTagPtOverMht/D')
-tEvent.Branch('TrackBdtScore', var_TrackBdtScore,'TrackBdtScore/D')
-tEvent.Branch('TrackChisquare', var_TrackChisquare,'TrackChisquare/D')
-tEvent.Branch('TrackDxy', var_TrackDxy,'TrackDxy/D')
-tEvent.Branch('TrackDeDx', var_TrackDeDx,'TrackDeDx/D')
+
+
 tEvent.Branch('CrossSection', var_CrossSection,'CrossSection/D')
 if isDasAndSignal: tEvent.Branch('weight', var_weight,'weight/D')
 
@@ -97,10 +122,12 @@ if isDasAndSignal: tEvent.Branch('weight', var_weight,'weight/D')
 ###########################
 
 readerPixelOnly = TMVA.Reader()
-pixelXml = '/nfs/dust/cms/user/kutznerv/cmsdas/BDTs/newpresel3-200-4-short-nodxyVtx/weights/TMVAClassification_BDT.weights.xml'
+#pixelXml = '/nfs/dust/cms/user/kutznerv/cmsdas/BDTs/newpresel3-200-4-short-nodxyVtx/weights/TMVAClassification_BDT.weights.xml'
+pixelXml = '/nfs/dust/cms/user/kutznerv/shorttrack/fake-tracks/newpresel3-200-4-short/weights/TMVAClassification_BDT.weights.xml'
 prepareReader(readerPixelOnly, pixelXml)
 readerPixelStrips = TMVA.Reader()
-trackerXml = '/nfs/dust/cms/user/kutznerv/cmsdas/BDTs/newpresel2-200-4-medium-nodxyVtx/weights/TMVAClassification_BDT.weights.xml'
+#trackerXml = '/nfs/dust/cms/user/kutznerv/cmsdas/BDTs/newpresel2-200-4-medium-nodxyVtx/weights/TMVAClassification_BDT.weights.xml'
+trackerXml = '/nfs/dust/cms/user/kutznerv/shorttrack/fake-tracks/newpresel2-200-4-medium/weights/TMVAClassification_BDT.weights.xml'
 prepareReader(readerPixelStrips, trackerXml)
 
 c = TChain('TreeMaker2/PreSelection')
@@ -129,102 +156,143 @@ for ientry in range(nentries):
     hHtWeighted.Fill(c.HT, weight)
 
 
-    if not (c.MHT>120): continue
+    if not (c.MET>120): continue
     if not (c.NJets>0): continue
     
     
     if 'TTJets_TuneCUET' in infilenames:
-	 if not c.madHT<600: continue
+     if not c.madHT<600: continue
     if 'TTJets_HT' in infilenames:
-    	if not c.madHT>600: continue  
+        if not c.madHT>600: continue  
     if 'WJetsToLNu_TuneCUET' in infilenames:
-    	if not c.madHT<100: continue
+        if not c.madHT<100: continue
     elif 'WJetsToLNu_HT' in infilenames:
-	 if not c.madHT>100: continue  	      
+     if not c.madHT>100: continue            
 
 
     var_Met[0] = c.MET
-    var_Mht[0] = c.MHT    
-    var_Mt2[0] = c.MT2    
-    var_Ht[0] = c.HT
 
     metvec = TLorentzVector()
     metvec.SetPtEtaPhiE(c.MET, 0, c.METPhi, c.MET)
     mhtvec = TLorentzVector()
-    mhtvec.SetPtEtaPhiE(c.MHT, 0, c.MHTPhi, c.MHT)
-    mindphi = 9999
-    nj = 0
+    mhtvec.SetPtEtaPhiE(0, 0, 0, 0)
+    jets = []
     nb = 0
+    ht = 0
     for ijet, jet in enumerate(c.Jets): #need dphi w.r.t. the modified mht
-        if not (abs(jet.Eta())<5.0 and jet.Pt()>30): continue
-        nj+=1
+        if not (abs(jet.Eta())<2.4 and jet.Pt()>30): continue
+        mhtvec-=jet
+        jets.append(jet)
+        ht+=jet.Pt()        
         if c.Jets_bDiscriminatorCSV[ijet]>csv_b: nb+=1
-        if abs(jet.DeltaPhi(mhtvec))<mindphi:
-            mindphi = abs(jet.DeltaPhi(mhtvec))
+    var_NJets[0] = len(jets)
+    var_Mht[0] = mhtvec.Pt()
+    mindphi = 9999   
+    for jet in jets: 
+    	if abs(jet.DeltaPhi(mhtvec))<mindphi:
+        	mindphi = abs(jet.DeltaPhi(mhtvec))
+            
+    var_Ht[0] = ht
     var_MinDeltaPhiMhtJets[0] = mindphi
-
-    var_NJets[0] = nj
     var_BTags[0] = nb
-    var_NLeptons[0] = len(c.Electrons)+len(c.Muons)
+    var_NLeptons[0] = c.NElectrons+c.NMuons
 
     sumtagvec = TLorentzVector()
     nPixelOnly = 0
     nPixelStrips = 0
-
-    mva, dedx, trkpt, trketa, trkp = -999, -999, -999, -999, -999
-    trkdxy, trkchisq = -888,-888
-    nprompt = 0
-    pt = -1
     
-    
+    mvas = []
+    trkpts = []
+    trketas = []
+    trkphis = []    
+    trkdxys = []
+    trkchisqs = [] 
+    dedxs = []
+    massfromdedxs = []
+    ispixelstripss = [0]
+    ismatcheds = [0]
+    ntags = 0
+    npix, npixstrips = 0, 0
     for itrack, track in enumerate(c.tracks):
-        if not track.Pt() > 15 : continue
-        if not abs(track.Eta()) < 2.4: continue
-        if abs(abs(track.Eta()) < 1.566) and abs(track.Eta()) > 1.4442: continue
-        mva_ = isDisappearingTrack_(track, itrack, c, readerPixelOnly, readerPixelStrips)
-        if not mva_: continue            
-        if mva_>mva: 
-        	mva = mva_
-        	dedx = c.tracks_deDxHarmonic2[itrack]
-        	trkpt = c.tracks[itrack].Pt()
-        	trkp = c.tracks[itrack].P()
-        	trketa = c.tracks[itrack].Eta()  
-        	trkdxy = abs(c.tracks_dxyVtx[itrack])
-        	trkchisq = abs(c.tracks_chi2perNdof[itrack])        
-        #if moh==0: print 'RunNum=%d, LumiBlockNum=%d, EvtNum=%d' % (c.RunNum, c.LumiBlockNum, c.EvtNum)
-        sumtagvec+=track
-        phits = c.tracks_nValidPixelHits[itrack]
-        thits = c.tracks_nValidTrackerHits[itrack]        
-        pixelOnly = phits>0 and thits==phits
-        pixelStrips = not pixelOnly
-        if pixelOnly: nPixelOnly+=1
-        if pixelStrips: nPixelStrips+=1
-        
-        genParticles = []
-        for igp, gp in enumerate(c.GenParticles):
-            if not gp.Pt()>3: continue        
-            if not abs(c.GenParticles_PdgId[igp]) in [11, 13, 211]: continue                    
-            if not c.GenParticles_Status[igp] == 1: continue
-            genpart = [gp.Clone(),-int(abs(c.GenParticles_PdgId[igp])/c.GenParticles_PdgId[igp])]
-            genParticles.append(genpart)        
+            if not track.Pt() > 15 : continue
+            if not abs(track.Eta()) < 2.4: continue
+            if abs(abs(track.Eta()) < 1.566) and abs(track.Eta()) > 1.4442: continue
+            mva_ = isDisappearingTrack_(track, itrack, c, readerPixelOnly, readerPixelStrips)
+            if not mva_: continue
+            ntags+=1
+            mvas.append(mva_)
+            trkpts.append(c.tracks[itrack].Pt())
+            trketas.append(c.tracks[itrack].Eta())
+            trkphis.append(c.tracks[itrack].Phi())
+            trkdxys.append(abs(c.tracks_dxyVtx[itrack]))
+            trkchisqs.append(c.tracks_chi2perNdof[itrack])
+            dedxs.append(c.tracks_deDxHarmonic2[itrack])   
+            massfromdedxs.append(TMath.Sqrt((dedxs[-1]-2.557)*pow(c.tracks[itrack].P(),2)/2.579))
+            #if moh==0: print 'RunNum=%d, LumiBlockNum=%d, EvtNum=%d' % (c.RunNum, c.LumiBlockNum, c.EvtNum)
+            sumtagvec+=track
+            phits = c.tracks_nValidPixelHits[itrack]
+            thits = c.tracks_nValidTrackerHits[itrack]        
+            pixelOnly = phits>0 and thits==phits
+            pixelStrips = not pixelOnly
+            if pixelStrips: 
+            	ispixelstripss.append(1)
+            	npix+=1
+            else: 
+            	ispixelstripss.append(0) 
+            	npixstrips+=1
+            genParticles = []
+            for igp, gp in enumerate(c.GenParticles):
+                if not gp.Pt()>3: continue        
+                if not abs(c.GenParticles_PdgId[igp]) in [11, 13, 211]: continue                    
+                if not c.GenParticles_Status[igp] == 1: continue
+                genpart = [gp.Clone(),-int(abs(c.GenParticles_PdgId[igp])/c.GenParticles_PdgId[igp])]
+                genParticles.append(genpart)        
+            if isMatched_([track, 0], genParticles, 0.01): ismatcheds.append(True)
+            else: ismatcheds.append(False)
             
-        if isMatched_([track, 0], genParticles, 0.01): nprompt+=1
-
-
-    var_TrackDxy[0] = trkdxy
-    var_TrackChisquare[0] = trkchisq 
-    var_NPixelTags[0] = nPixelOnly
-    var_NPixelStripsTags[0] = nPixelStrips
-    var_NTags[0] = nPixelOnly+nPixelStrips
-    if not var_NTags[0]>0: continue
-
-    var_NPrompt[0] = nprompt
+    if len(mvas)==0: continue
+    if len(mvas)==1:
+        var_Track1BdtScore[0] = mvas[0]
+        var_Track1Pt[0] = trkpts[0]
+        var_Track1Eta[0] = trketas[0]
+        var_Track1Phi[0] = trkphis[0]
+        var_Track1Chisquare[0] = trkchisqs[0]
+        var_Track1Dxy[0] = trkdxys[0]        
+        var_Track1Dedx[0] = dedxs[0] 
+        var_Track1MassFromDedx[0] = massfromdedxs[0] 
+        var_Track1IsPixelStrips[0] = ispixelstripss[0]
+        var_Track1IsGenMatched[0] = ismatcheds[0]   
+        
+        var_Track2BdtScore[0] = -11
+        var_Track2Pt[0] = -11
+        var_Track2Eta[0] = -11
+        var_Track2Phi[0] = -11
+        var_Track2Chisquare[0] = -11
+        var_Track2Dxy[0] = -11
+        var_Track2Dedx[0] = -11
+        var_Track2MassFromDedx[0] = -11
+        var_Track2IsPixelStrips[0] = -11
+        var_Track2IsGenMatched[0] = -11
+                 
+    if len(mvas)>1:
+        var_Track2BdtScore[0] = mvas[1]
+        var_Track2Pt[0] = trkpts[1]
+        var_Track2Eta[0] = trketas[1]
+        var_Track2Phi[0] = trkphis[1]
+        var_Track2Chisquare[0] = trkchisqs[1]
+        var_Track2Dxy[0] = trkdxys[1]        
+        var_Track2Dedx[0] = dedxs[1] 
+        var_Track2MassFromDedx[0] = massfromdedxs[1] 
+        var_Track2IsPixelStrips[0] = ispixelstripss[1]
+        var_Track2IsGenMatched[0] = ismatcheds[1]   
+    var_NTags[0] = ntags
+    var_NPixelTags[0] = npix
+    var_NPixelStripsTags[0] = npixstrips        
+        
+        
     var_DPhiMetSumTags[0] = abs(mhtvec.DeltaPhi(sumtagvec))
     var_SumTagPtOverMht[0] = sumtagvec.Pt()/mhtvec.Pt()
-    var_TrackBdtScore[0] = mva
-    var_TrackPt[0] = trkpt
-    var_TrackP[0] = trkp
-    var_TrackEta[0] = trketa        
+
     
     var_CrossSection[0] = c.CrossSection
     tEvent.Fill()
